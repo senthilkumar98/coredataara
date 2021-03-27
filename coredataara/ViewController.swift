18e8c3543fcebbd89a0224f63886c0faad630f54
SathishMuthu96 <sathishmuthu2911@gmail.com>
2021-03-27 13:02:25 +0530

    new update

---
+++ coredataara/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  coredataara
+//
+//  Created by apple on 04/01/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+import  CoreData
+class ViewController: UIViewController
+{
+    @IBOutlet weak var t1: UITextField!
+    @IBOutlet weak var t2: UITextField!
+    
+    @IBAction func pass(_ sender: Any)
+    {
+        SaveRecord()
+        performSegue(with
+    }
+    func SaveRecord()
+    {
+        
+    }
+    override func viewDidLoad()
+    {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ coredataara/secViewController.swift
@@ -0,0 +1,32 @@
+//
+//  secViewController.swift
+//  coredataara
+//
+//  Created by apple on 04/01/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class secViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
